a49b724c35f2b01b0b5115633798e550cf1c30bc
batumzff <batuhanmuzafferoglu@gmail.com>
2023-11-29 23:02:38 +0300

    2

---
+++ clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc02_comp-props-style/comp-jsx/src/App.js
@@ -0,0 +1,18 @@
+import Footer from "./Footer"
+import Header from "./Header"
+import { Main } from "./Main"
+
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1>React App</h1>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App

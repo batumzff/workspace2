9ff6687fcca658c625dcb84b5d60d2bf11530d1f
batumzff <batuhanmuzafferoglu@gmail.com>
2023-12-01 19:16:19 +0300

    sass

--- clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc03_module-css/src/data.js
+++ clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc03_module-css/src/data.js
@@ -1,3 +1,4 @@
+//? JSON (mock data)
 const data = [
   {
     id: 1,
@@ -16,6 +17,12 @@ const data = [
     language: "HTML",
     img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png",
     btnName: "More",
+  },
+  {
+    id: 4,
+    language: "Go",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png",
+    btnName: "Git",
   },
 ]
 export default data

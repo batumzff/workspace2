12d2195966e348659d4b4f6df75ae5d1ad93a006
batumzff <batuhanmuzafferoglu@gmail.com>
2023-12-04 23:14:40 +0300

    last

---
+++ clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc05_events-useStateHook/src/3-useState/UseStateObject.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react"
+
+const UseStateObject = () => {
+  //   const [ad, setAd] = useState("Ahmet")
+  //   const [meslek, setMeslek] = useState("Developer")
+  //   const [yas, setYas] = useState(22)
+
+  const [kisi, setKisi] = useState({
+    ad: "Canan",
+    meslek: "Developer",
+    yas: 22,
+    maas: 50000,
+  })
+  //   const { ad, meslek, yas } = kisi //? state destr yapildi.
+
+  //   let toggle = false
+  //? Boolean state
+  const [toggle, setToggle] = useState(false)
+
+  const handleSwap = () => {
+    // toggle = !toggle
+    setToggle(!toggle)
+    console.log(toggle)
+    if (toggle) {
+      setKisi({
+        ad: "Canan",
+        meslek: "Developer",
+        yas: 22,
+        maas: 200000,
+      })
+    } else {
+      setKisi({
+        ad: "Ali",
+        meslek: "Tester",
+        yas: 42,
+        maas: 100000,
+      })
+    }
+  }
+
+  console.log(kisi)
+  return (
+    <div>
+      <h1>Personel Formu</h1>
+      <h2>Adi:{kisi.ad}</h2>
+      <h3>Meslegi:{kisi.meslek}</h3>
+      <h3>Yasi:{kisi.yas}</h3>
+      <h3>Maas:{kisi.maas}</h3>
+
+      <button onClick={() => setKisi({ ...kisi, ad: "Mehmet" })}>
+        isim degistir
+      </button>
+      <button onClick={() => setKisi({ ...kisi, meslek: "Tester" })}>
+        Meslek degistir
+      </button>
+      <button onClick={() => setKisi({ ...kisi, yas: kisi.yas + 1 })}>
+        Yas Arttir
+      </button>
+
+      <button onClick={() => setKisi({ ad: "Can", meslek: "PO", yas: 52 })}>
+        Tamamini Guncelle
+      </button>
+
+      <button onClick={handleSwap}>Swap</button>
+    </div>
+  )
+}
+
+export default UseStateObject

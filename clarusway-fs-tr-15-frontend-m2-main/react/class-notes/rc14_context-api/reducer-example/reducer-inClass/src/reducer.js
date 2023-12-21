6b525fb5709ba25f0c0acb7286cc476a879ef2cd
batumzff <batuhanmuzafferoglu@gmail.com>
2023-12-21 23:23:35 +0300

    reducer

---
+++ clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc14_context-api/reducer-example/reducer-inClass/src/reducer.js
@@ -0,0 +1,23 @@
+export const initialState = {
+    loading: false,
+    catImage: "",
+    error: "",
+  }
+  
+  export const reducer = (initialState, action) => {
+    switch (action.type) {
+      case "START":
+        return { ...initialState, loading: true }
+      case "SUCCESS":
+        return {
+          ...initialState,
+          catImage: action.payload,
+          loading: false,
+          error: "",
+        }
+  
+      default:
+        break
+    }
+  }
+  

0e35b8b9c5ed81536e17bbebe5898925b463175a
batumzff <batuhanmuzafferoglu@gmail.com>
2023-12-21 21:14:16 +0300

    login

--- clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc13_tailwind/src/App.js
+++ clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc13_tailwind/src/App.js
@@ -1,7 +1,14 @@
+import SpacingAndSizing from "./components/SpacingAndSizing";
+import Typografy from "./components/Typografy";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Tailwind</h1>
+      <h1 className="text-2xl text-center text-purple-700 font-bold">
+        Hello Tailwind
+      </h1>
+      {/* <Typografy /> */}
+      <SpacingAndSizing />
     </div>
   );
 }
---
+++ clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc13_tailwind/src/components/Typografy.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Typografy = () => {
+  return (
+    <div>
+      <h3 className="text-2xl">Typografy</h3>
+      <p className="text-sm">Typografy</p>
+      <p className="text-base/7">Typografy</p>
+      <p className="text-lg">Typografy</p>
+      <p className="text-xl">Typografy</p>
+      <p className="text-2xl">Typografy</p>
+      <h2 className="text-3xl">Başlik</h2>
+      <h1 className="text-[60px]">Başlik</h1>
+      <article>
+        <p className="line-clamp-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          fugiat minus ratione architecto in voluptatem dignissimos deleniti
+          vero sed suscipit? Ipsam rerum dolorem iure, corrupti praesentium a
+          quos. Officia, consequuntur! Adipisci dolor similique quibusdam sed
+          nisi iusto fugit odit cumque deleniti suscipit rem laboriosam modi
+          asperiores placeat eaque facilis blanditiis natus porro repellat quae
+          saepe, eum earum velit ut. Mollitia distinctio vitae deserunt neque
+          tempore magnam nostrum, voluptate tenetur assumenda, consectetur
+          laudantium possimus ab veritatis ratione eum dolor impedit sint, iusto
+          alias placeat ipsa aspernatur quis? Velit, sunt. Cum eius deleniti
+          distinctio. Quisquam nam consequuntur eius omnis id nisi, adipisci
+          consectetur enim deleniti fugiat, magnam aperiam inventore in itaque
+          quas dolorum nulla quo accusamus ducimus facilis obcaecati ipsa. Quia,
+          eum maiores dolores dignissimos vel perspiciatis libero fugit ut
+          accusantium sed numquam magnam omnis provident dolore! Officiis
+          voluptatem laborum repudiandae. Asperiores eveniet hic autem unde quos
+          iste eum expedita necessitatibus quis!
+        </p>
+      </article>
+      <p className="text-center">Merhaba</p>
+      <p className="underline">Merhaba</p>
+      <p className="line-through">Merhaba</p>
+      <p className="uppercase">Merhaba</p>
+      <p className="lowercase">Merhaba</p>
+      <p className="text-red-400">Merhaba</p>
+      <p className="text-[#A1EEBD]">Merhaba</p>
+      <p className="text-[rgba(73,163,186,0.886)]">Merhaba</p>
+    </div>
+  );
+};
+
+export default Typografy;

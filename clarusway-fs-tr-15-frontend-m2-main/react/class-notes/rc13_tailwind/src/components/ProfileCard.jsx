23045cb6f9ed6db50a356027c689b1abeeb9c4ba
batumzff <batuhanmuzafferoglu@gmail.com>
2023-12-22 22:05:08 +0300

    indexcss

--- clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc13_tailwind/src/components/ProfileCard.jsx
+++ clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc13_tailwind/src/components/ProfileCard.jsx
@@ -9,19 +9,13 @@ const ProfileCard = ({ id }) => {
           src={`https://api.dicebear.com/7.x/avataaars/svg?seed=${id}`}
           alt="Bonnie image"
         />
-        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-          Bonnie Green
-        </h5>
+        <h5>Bonnie Green</h5>
         <span className="text-sm text-gray-500 dark:text-gray-400">
           Visual Designer
         </span>
         <div className="flex mt-4 md:mt-6">
-          <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Add friend
-          </button>
-          <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">
-            Message
-          </button>
+          <button className="btn-primary">Add friend</button>
+          <button className="btn-secondary">Message</button>
         </div>
       </div>
     </div>

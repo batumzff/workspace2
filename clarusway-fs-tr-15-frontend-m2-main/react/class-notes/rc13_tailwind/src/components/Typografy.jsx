59c785fcc053e0bbd7f06b1b867d777393ab7601
batumzff <batuhanmuzafferoglu@gmail.com>
2023-12-22 21:56:14 +0300

    tailwind

--- clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc13_tailwind/src/components/Typografy.jsx
+++ clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc13_tailwind/src/components/Typografy.jsx
@@ -9,27 +9,22 @@ const Typografy = () => {
       <p className="text-lg">Typografy</p>
       <p className="text-xl">Typografy</p>
       <p className="text-2xl">Typografy</p>
-      <h2 className="text-3xl">Başlik</h2>
-      <h1 className="text-[60px]">Başlik</h1>
+      <h2 className="text-3xl">Başlık</h2>
+      <h1 className="text-[60px]">Başlık</h1>
       <article>
         <p className="line-clamp-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-          fugiat minus ratione architecto in voluptatem dignissimos deleniti
-          vero sed suscipit? Ipsam rerum dolorem iure, corrupti praesentium a
-          quos. Officia, consequuntur! Adipisci dolor similique quibusdam sed
-          nisi iusto fugit odit cumque deleniti suscipit rem laboriosam modi
-          asperiores placeat eaque facilis blanditiis natus porro repellat quae
-          saepe, eum earum velit ut. Mollitia distinctio vitae deserunt neque
-          tempore magnam nostrum, voluptate tenetur assumenda, consectetur
-          laudantium possimus ab veritatis ratione eum dolor impedit sint, iusto
-          alias placeat ipsa aspernatur quis? Velit, sunt. Cum eius deleniti
-          distinctio. Quisquam nam consequuntur eius omnis id nisi, adipisci
-          consectetur enim deleniti fugiat, magnam aperiam inventore in itaque
-          quas dolorum nulla quo accusamus ducimus facilis obcaecati ipsa. Quia,
-          eum maiores dolores dignissimos vel perspiciatis libero fugit ut
-          accusantium sed numquam magnam omnis provident dolore! Officiis
-          voluptatem laborum repudiandae. Asperiores eveniet hic autem unde quos
-          iste eum expedita necessitatibus quis!
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero
+          reiciendis, saepe quibusdam voluptatem ipsum inventore vitae ducimus
+          similique dolorem veniam debitis quae dolorum sapiente exercitationem
+          ipsam atque molestias quo alias. Unde animi nostrum a distinctio dicta
+          quisquam, exercitationem velit ea cupiditate illo eaque cum sit
+          laborum dolor quod veniam assumenda repellat ipsa odit necessitatibus
+          fugiat eum quos? Iure, doloremque explicabo error minima aperiam
+          excepturi quod odio sapiente. Officiis dolorum impedit, temporibus
+          consequuntur odit at maiores! Exercitationem necessitatibus ea
+          corporis consectetur aliquam facilis, corrupti sapiente quae iste,
+          quidem quisquam impedit accusantium eum velit tenetur id accusamus ut
+          expedita alias laudantium itaque?
         </p>
       </article>
       <p className="text-center">Merhaba</p>

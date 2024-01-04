f6bd0b91d8bff3dc4b0dfddfde9a388d4852b0ef
batumzff <batuhanmuzafferoglu@gmail.com>
2024-01-04 23:22:58 +0300

    redux project

--- clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc20_redux-intro/redux-intro-inClass/src/store/store.jsx
+++ clarusway-fs-tr-15-frontend-m2-main/react/class-notes/rc20_redux-intro/redux-intro-inClass/src/store/store.jsx
@@ -1,13 +1,16 @@
-import { legacy_createStore as createStore, combineReducers } from "redux"
-import { counterReducer } from "./counterReducer"
-import { todoReducer } from "./todoReducer"
-import { composeWithDevTools } from "@redux-devtools/extension"
+import { legacy_createStore as createStore, combineReducers } from "redux";
+import { counterReducer } from "./counterReducer";
+import { todoReducer } from "./todoReducer";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 // export const store = createStore(counterReducer)
 
 const rootReducer = combineReducers({
   counter: counterReducer,
   todo: todoReducer,
-})
+});
 
-export const store = createStore(rootReducer, composeWithDevTools())
+export const store = createStore(
+  rootReducer,
+  process.env.NODE_ENV === "development" && composeWithDevTools()
+);
